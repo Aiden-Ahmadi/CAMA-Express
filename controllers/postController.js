d28fcf3d88cdc51a93f4d5eedb2326b5fc2fa9dc
Aiden <aidengm3565@gmail.com>
2025-03-13 14:31:58 -0700

    fixed bugs

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,11 +1,11 @@
-const Post = require("../models/Post");
+const Post = require("../models/post");
 const User = require("../models/User");
 
 const getUserPosts = async (req, res) => {
     try {
         const posts = await Post.findById(req.params.id);
         if (!posts) return res.status(404).json({ message: "User not found" });
-        res.json(user);
+        res.json(posts);
     } catch (error) {
         res.status(500).json({ error: "Server error" });
     }
