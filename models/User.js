642c50b5c67916c93fbbf2d216fc24bb9fa47e03
Aiden <aidengm3565@gmail.com>
2025-03-16 22:33:41 -0700

    Added password to user schema

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
+  password: {type: String, required: true},
   followerIds: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
   followingIds: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
   profileImage: { type: String, default: "" }, // Optional profile picture
